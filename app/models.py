bdadffb7ce95344333198448fb981e0e8fad79ed
mnferreira <mnferreira8@gmail.com>
2019-05-19 01:08:44 -0300

    adição formulário cadastro

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Cadastro (models.Model):
+
+    nome = models.CharField(max_length=120)
+    email = models.EmailField()
+    CPF = models.IntegerField()
+    login = models.CharField(max_length=50)
+    senha = models.CharField(max_length=50)
+
+def __str__(self):
+        return self.nome
--- app/views.py
+++ app/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from app.forms import CadastroForm
 from app import views
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def fazer_cadastro(request):
+    formulario = CadastroForm(request.POST or None)
+    msg = ''
+    if formulario.is_valid():
+        formulario.save()
+        formulario = CadastroForm()
+        
+    contexto = {
+        'form' : formulario,
+    }
+
+    return render(request, 'cadastro.html', contexto)
